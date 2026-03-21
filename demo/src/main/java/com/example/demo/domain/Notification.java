826ba1581d2a7cbfa8a4632a132b5a7db6f30c6a
Bellito29 <juanbello2029@gmail.com>
2026-03-21 12:32:09 -0500

    modelnotifications to stats impl

--- demo/src/main/java/com/example/demo/domain/Notification.java
+++ demo/src/main/java/com/example/demo/domain/Notification.java
@@ -1,5 +1,26 @@
 package com.example.demo.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Notification {
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "user_id")
+    private Integer userId;
+    @Column(name = "reference_id")
+    private Integer referenceId;
+
+    private String type;
 }
